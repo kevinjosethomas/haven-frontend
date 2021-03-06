0f0928b52d69ee687c262b38df4bafd0fa7f0e05
DevAnaxes <DevAnaxes@users.noreply.github.com>
2021-03-07 08:44:47 +1000

    Updated href - Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,10 +11,10 @@ export const Footer = (props) => {
             <a target="_blank" href="/linkedin" className="mr-7 fab fa-linkedin text-3xl text-gray-500"></a>
         </div>
             <div class="px-2 pt-2 pb-3 space-y-1">
-                <span className={`cursor-pointer font-lg font-bold ${router.pathname == "/team" ? "text-gray-500" : "text-gray-500"} mr-6`} >Team</span>
-                <span className={`cursor-pointer font-lg font-bold ${router.pathname == "/partners" ? "text-gray-500" : "text-gray-500 "} mr-6`} >Partners</span>
-                <span className={`cursor-pointer font-lg font-bold ${router.pathname == "/privacy" ? "text-gray-500" : "text-gray-500"} mr-6`} >Privacy Policy</span>
-                <span className={`cursor-pointer font-lg font-bold ${router.pathname == "/terms" ? "text-gray-500" : "text-gray-500"} mr-6`} >Terms</span>
+                <span className="cursor-pointer font-lg font-bold text-gray-500 mr-6" href="/team">Team</span>
+                <span className="cursor-pointer font-lg font-bold text-gray-500 mr-6" href="/partners">Partners</span>
+                <span className="cursor-pointer font-lg font-bold text-gray-500 mr-6" href="/privacy">Privacy Policy</span>
+                <span className="cursor-pointer font-lg font-bold text-gray-500 mr-6" href="/terms">Terms</span>
                 </div>
         <p className="font-medium text-medium inter text-gray-500 font-bold">developed by <a href="https://codebyte.team/" target="_blank" className="darktext font-bold">teamcodebyte</a> and our <a href="https://github.com/teamcodebyte" target="_blank" className="darktext font-bold">opensource community</a></p>
         <div className="mb-16"></div>
