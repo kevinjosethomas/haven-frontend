c955824faa3622ebe5e5e114016525300c08d2a2
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-13 16:20:28 -0500

    Added home data with value cards

---
+++ src/utility/data/home.js
@@ -0,0 +1,33 @@
+
+export const ValueCards =  [
+  {
+    icon: "fas fa-id-card",
+    title: "Quirky Profiles",
+    content: "Create descriptive profiles from a range of customizable themes to express yourself and connect your online presence."
+  },
+  {
+    icon: "far fa-link",
+    title: "Vanity Links",
+    content: "Providing free vanity links for your profiles and any websites, blogs or redirects that you want to show on posts and your bio!"
+  },
+  {
+    icon: "far fa-code-merge",
+    title: "Opensource",
+    content: "Developed by the community, we’re as transparent as we can be. Get to know everything that’s going on behind the scenes."
+  },
+  {
+    icon: "fas fa-plug",
+    title: "Integrated",
+    content: "Connect all your social media accounts in one place to make it easier for people to find you where you want to be found."
+  },
+  {
+    icon: "fas fa-project-diagram",
+    title: "Connected",
+    content: "Making it easier to meet other likeminded people from all around the world. Easier to make new friends or get work done!"
+  },
+  {
+    icon: "fas fa-door-open",
+    title: "Inclusive",
+    content: "Building a safe community, inclusive of everyone. Active Trust & Safety team, auto moderation and a lot more for your safety!"
+  }  
+];
