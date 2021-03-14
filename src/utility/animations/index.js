665db0f097887db7f59a5dfac429c876bb77e38c
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-14 00:21:19 -0500

    Added components animations file

--- src/utility/animations/index.js
+++ src/utility/animations/index.js
@@ -1,14 +1,4 @@
 
-export const NavbarButtonHover = {
-  initial: {
-    x: 0
-  },
-  hover: {
-    x: 4,
-    transition: { duration: 0.4 }
-  }
-}
-
 export const TargetCardHover = {
   hover: {
     scale: 1.2,
