f4ddd75ce130992ef61ffd90228f9c8b3be800ae
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-14 00:22:17 -0500

    Added neumorphic drop shadow

--- src/components/index/TargetCard.js
+++ src/components/index/TargetCard.js
@@ -2,15 +2,15 @@
 export const TargetCard = props => {
 
   return (
-    <div className="TargetCard flex flex-col items-center justify-between mx-12 w-96 w-card-target-large h-card-target-large">
-      <div className="flex flex-row items-end justify-start px-10 py-6 w-full h-1/4 bg-light-400 bg-opacity-90">
-        <i className={`${props.icon} mr-1 text-5xl text-gray-700`} />
+    <div className="TargetCard flex flex-col items-center justify-between mx-12 w-96 w-card-target-large h-card-target-large rounded">
+      <div className="flex flex-row items-end justify-start px-10 py-6 w-full h-1/4 bg-light-300 bg-opacity-30 rounded-t">
+        <i className={`${props.icon} mr-1 text-5xl ${props.color}`} />
         <h1 className="ml-1 font-acumin font-semibold text-5xl text-gray-700">{ props.title }</h1>
       </div>
-      <div className="flex flex-col items-start justify-center px-10 w-full h-3/4 space-y-5 bg-light-400 bg-opacity-50">
+      <div className="flex flex-col items-start justify-center px-10 w-full h-3/4 space-y-5 rounded-b">
         { props.content.map(item => (
           <p className="font-inter font-medium text-2xl text-gray-500">
-            <span className="text-gray-600">•</span> <span className={item.decoration}>{ item.text }</span>
+            <span className={props.color}>•</span> <span className={item.decoration}>{ item.text }</span>
           </p>
         ))}
       </div>
