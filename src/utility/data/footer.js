451bb49c4674634a3f0ae36b0c86f41b3591b764
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-15 17:55:03 -0400

    Added data for footer links

---
+++ src/utility/data/footer.js
@@ -0,0 +1,38 @@
+
+export const Socials = [
+  {
+    icon: "fab fa-github",
+    href: "https://github.haven.bio"
+  },
+  {
+    icon: "fab fa-discord",
+    href: "https://discord.haven.bio"
+  },
+  {
+    icon: "fab fa-twitter",
+    href: "https://twitter.haven.bio"
+  },
+  {
+    icon: "fab fa-linkedin",
+    href: "https://www.linkedin.com/company/teamcodebyte"
+  }
+];
+
+export const Links = [
+  {
+    label: "Team",
+    href: "/team"
+  },
+  {
+    label: "Partners",
+    href: "/partners"
+  },
+  {
+    label: "Privacy Policy",
+    href: "/privacy"
+  },
+  {
+    label: "Terms",
+    href: "/terms"
+  },
+];
