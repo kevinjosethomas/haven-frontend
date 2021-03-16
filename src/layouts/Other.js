8ad69fe8f7f33f8c101fc40f0373c06e58bacd5a
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-16 18:51:03 -0400

    Added Other layout

--- src/layouts/Other.js
+++ src/layouts/Other.js
@@ -2,7 +2,7 @@
 export const Layout = props => {
 
   return (
-    <div className="flex flex-col min-h-screen overflow-x-hidden">
+    <div className="flex flex-col h-screen min-h-screen overflow-x-hidden">
       { props.children }
     </div>
   )
