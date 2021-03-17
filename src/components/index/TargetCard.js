d33b0c60ef82eb4cd73fd3dc396c5d667a412790
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-17 19:10:53 -0400

    Changed margin system

--- src/components/index/TargetCard.js
+++ src/components/index/TargetCard.js
@@ -2,7 +2,7 @@
 export const TargetCard = props => {
 
   return (
-    <div className="TargetCard flex flex-col items-center justify-between mx-12 w-96 w-card-target-large h-card-target-large cursor-default rounded transform transition duration-500 hover:scale-103">
+    <div className="TargetCard flex flex-col items-center justify-between w-96 w-card-target-large h-card-target-large cursor-default rounded transform transition duration-500 hover:scale-103">
       <div className="flex flex-row items-end justify-start px-10 py-6 w-full h-1/4 bg-light-300 bg-opacity-50 rounded-t">
         <i className={`${props.icon} mr-1 text-5xl ${props.color}`} />
         <h1 className="ml-1 font-acumin font-semibold text-5xl text-gray-700">{ props.title }</h1>
