f12735a6fedc8dc5949f9fa13c6416e02b0f45d8
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-17 19:34:13 -0400

    Made Feature section responsive

--- src/layouts/sections/Feature.js
+++ src/layouts/sections/Feature.js
@@ -2,14 +2,14 @@
 export const FeatureSection = props => {
 
   return (
-    <div className={`flex flex-row items-center justify-center w-full h-section ${props.background}`}>
-      <div className="flex flex-col items-start justify-center mr-10 max-w-5xl">
-        <h1 className="w-min mb-2 font-acumin font-bold xl:text-9xl 2xl:text-11xl text-gray-800 tracking-tight">{ props.title }</h1>
-        <p className="mt-2 max-w-3xl font-inter font-medium xl:text-3xl 2xl:text-4xl text-gray-700">{ props.description }</p>
+    <div className={`flex flex-row items-center justify-center w-full h-section-lg xl:h-section ${props.background}`}>
+      <div className="flex flex-col items-start justify-center mr-10 max-w-xl xl:max-w-5xl">
+        <h1 className="w-min mb-2 font-acumin font-bold text-8xl xl:text-9xl 2xl:text-11xl text-gray-800 tracking-tight">{ props.title }</h1>
+        <p className="mt-2 max-w-3xl font-inter font-medium text-2xl xl:text-3xl 2xl:text-4xl text-gray-700">{ props.description }</p>
       </div>
       <img 
         draggable="false"
-        className="ml-10"
+        className="ml-10 w-80 xl:w-auto"
         alt="Another cool image that shouldn't be missing"
         src={props.image}
       />
