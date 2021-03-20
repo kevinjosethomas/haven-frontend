3f50e443c92fd9dbe1b2d6279cfa8bd6b0146e5e
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-20 10:06:09 -0400

    Made ValueCard slightly more responsive

--- src/components/index/ValueCard.js
+++ src/components/index/ValueCard.js
@@ -2,10 +2,10 @@
 export const ValueCard = props => {
 
   return (
-    <div className="flex flex-col items-start justify-center m-4 p-8 w-80 xl:w-96 h-80 xl:h-96 space-y-2 xl:space-y-4 bg-dark-200">
-      <i className={`${props.icon} text-4xl xl:text-5xl text-gray-300`} />
-      <h2 className="font-inter font-bold text-3xl xl:text-4xl text-gray-300">{props.title}</h2>
-      <p className="font-inter font-medium text-xl xl:text-2xl text-gray-400">{props.content}</p>
+    <div className="flex flex-col items-start justify-center m-4 p-8 w-64 xl:w-80 3xl:w-96 h-64 xl:h-80 3xl:h-96 space-y-2 3xl:space-y-4 bg-dark-200">
+      <i className={`${props.icon} text-2xl xl:text-4xl 3xl:text-5xl text-gray-300`} />
+      <h2 className="font-inter font-bold text-xl xl:text-3xl 3xl:text-4xl text-gray-300">{props.title}</h2>
+      <p className="font-inter font-medium text-md xl:text-xl 3xl:text-2xl text-gray-400">{props.content}</p>
     </div>
   )
 
