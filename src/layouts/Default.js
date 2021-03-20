7ced313d7e428419ef98b79b6c2c421b7ef649f9
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-20 19:29:32 -0400

    Loaded Banner to homepage

--- src/layouts/Default.js
+++ src/layouts/Default.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 
+import { Banner } from "../components/global/Banner";
 import { Navbar } from "../components/global/Navbar";
 
 export const Layout = props => {
@@ -8,6 +9,12 @@ export const Layout = props => {
 
   return (
     <div className="flex flex-col min-h-screen overflow-x-hidden">
+      <Banner
+        icon="twa twa-sparkles"
+        label="Join Discord"
+        href="https://discord.haven.bio"
+        message="Haven is currently under development, join our Discord server to keep in touch and win some giveaways!"
+      />
       <Navbar home={router.pathname == "/"} />
       { props.children }
     </div>
