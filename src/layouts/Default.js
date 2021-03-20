08a1cc7da0885a11eb8ab01a0bab0897a0cfa9a1
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-20 19:50:50 -0400

    Added animations to banner

--- src/layouts/Default.js
+++ src/layouts/Default.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
+import { AnimatePresence } from "framer-motion";
 
 import { Banner } from "../components/global/Banner";
 import { Navbar } from "../components/global/Navbar";
@@ -6,15 +8,21 @@ import { Navbar } from "../components/global/Navbar";
 export const Layout = props => {
 
   const router = useRouter();
+  const [bannerStatus, setBannerStatus] = useState(true);
 
   return (
     <div className="flex flex-col min-h-screen overflow-x-hidden">
-      <Banner
-        icon="twa twa-sparkles"
-        label="Join Discord"
-        href="https://discord.haven.bio"
-        message="Haven is currently under development, join our Discord server to keep in touch and win some giveaways!"
-      />
+      <AnimatePresence>
+        { bannerStatus && (
+        <Banner
+          icon="twa twa-sparkles"
+          label="Join Discord"
+          href="https://discord.haven.bio"
+          message="Haven is currently under development, join our Discord server to keep in touch and win some giveaways!"
+          setBannerStatus={setBannerStatus}
+        />
+      )}
+      </AnimatePresence>
       <Navbar home={router.pathname == "/"} />
       { props.children }
     </div>
