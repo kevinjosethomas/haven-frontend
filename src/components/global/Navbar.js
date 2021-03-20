a64ac20abc2cfe595cc24869af882eced8543697
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-20 10:40:41 -0400

    Made Navbar slightly more responsive

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -12,21 +12,21 @@ export const Navbar = ({ props, home }) => {
         <Logo
           color="white"
           type="wordmark"
-          width="w-44"
+          width="w-40 lg:w-44"
           href="/"
         />
       </div>
       <div className="flex flex-row items-center justify-end space-x-4">
         <Button
           label="Login"
-          width="w-28"
+          width="w-24 lg:w-28"
           href="/login"
           bgColor="gray-300"
           hoverBgColor="gray-400"
         />
         <Button
           label="Sign Up"
-          width="w-36"
+          width="w-28 lg:w-36"
           href="/register"
           bgColor="blue-400"
           hoverBgColor="blue-500"
@@ -42,13 +42,13 @@ export const Button = props => {
   return (
     <Link href={props.href}>
       <motion.div
-        className={`flex flex-row justify-center items-center px-5 py-2 ${props.width} rounded
+        className={`flex flex-row justify-center items-center py-2 ${props.width} rounded
         bg-${props.bgColor} hover:bg-${props.hoverBgColor} transition duration-300 cursor-pointer`}
         whileHover="hover"
       >
-        <span className="font-inter font-semibold text-xl text-white">{props.label}</span>
+        <span className="font-inter font-semibold text-lg lg:text-xl text-white">{props.label}</span>
         <motion.i
-          className={`far fa-angle-right text-lg text-white pl-2`}
+          className={`far fa-angle-right text-md lg:text-lg text-white pl-2`}
           initial="initial"
           variants={NavbarButtonHover}
         />
